0befc530825cdd4a2cd0f54536fbb55a70b0165f
karanpuri88 <32450274+karanpuri88@users.noreply.github.com>
2021-12-16 08:39:52 +0530

    Add files via upload

---
+++ Complex_number.py
@@ -0,0 +1,7 @@
+import cmath
+# Complex number denoted as x + yi where x is real number & y is imaginary number
+x = 5
+y = 6
+z = complex(x,y)
+print ("Real number is :" , z.real)
+print ("Imaginary number is :" ,z.imag)
