d77033f9dd919ee845fb36b5a64102e382df979d
karanpuri88 <32450274+karanpuri88@users.noreply.github.com>
2021-12-16 08:44:13 +0530

    Update API_call_Get_vManage.py

--- API_call_Get_vManage.py
+++ API_call_Get_vManage.py
@@ -4,14 +4,14 @@ import json
 from requests.auth import HTTPBasicAuth
 
 # Target API 
-url = "https://10.127.198.42:8443/dataservice/device/crashlog/synced?deviceId=100.100.100.1"
+url = "https://1.1.1.1:8443/dataservice/device/crashlog/synced?deviceId=2.2.2.2"
 
 #Faking as Browser 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"}
 
 # Ignore https certificate by verifying False & then pass username & password 
 
-response = requests.get(url, headers=headers, verify=False, auth=HTTPBasicAuth('karpuri', 'karpuri'))
+response = requests.get(url, headers=headers, verify=False, auth=HTTPBasicAuth('test', 'test'))
 
 # response of API call  
 
