f9ac6a3ce262fd0bb03652345dfadaf1387ebc7d
karanpuri88 <32450274+karanpuri88@users.noreply.github.com>
2022-01-10 23:09:30 +0530

    Update ssh_net.py

--- ssh_net.py
+++ ssh_net.py
@@ -1,7 +1,7 @@
 import netmiko
 
 # Take all inputs from User 
-ip_address = input("Enter IP address")
+ip_address = input("Enter IP address :")
 user = input("Enter Username :")
 pas = input("Enter Password :")
 
@@ -9,7 +9,7 @@ pas = input("Enter Password :")
 session = netmiko.ConnectHandler(ip = ip_address, username = user, password = pas, device_type = "cisco_xe")
 
 # Enter command to run & print it 
-command = input("Enter Command to run")
+command = input("Enter Command to run :")
 print(session.send_command(command + "\n"))
 
 #Disconnect Session
